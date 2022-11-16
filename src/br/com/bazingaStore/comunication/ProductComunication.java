ecbc31926e91399ecef25e6c34b660438c969565
AndersonDiasSampaio <anderson.dias.sampaio@gmail.com>
2022-11-16 13:31:03 -0300

    DeleteProduct by Sku

---
+++ src/br/com/bazingaStore/comunication/ProductComunication.java
@@ -0,0 +1,9 @@
+package br.com.bazingaStore.comunication;
+
+public class ProductComunication {
+
+	public ProductComunication() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/br/com/bazingaStore/service/StockService.java
+++ src/br/com/bazingaStore/service/StockService.java
@@ -19,45 +19,56 @@ public class StockService {
 
 	public String registProduct(Integer quantity, Double price, String department, String description, Color color,
 			Type type, Size Size, Category category, Sku Sku) {
-				Product product = new Product(quantity, price, department, description, color, type, Size, category, Sku);
-				List<Object> Stock= DataStock.listItens();
-				boolean isNew=true;
-				for(int x=0; x<Stock.size();x++) {
-					Product product2 = (Product) Stock.get(x);
-					if(product.getSku()==product2.getSku()) {
-						product2.setCategory(product.getCategory());
-						product2.setColor(product.getColor());
-						product2.setDepartment(product.getDepartment());
-						product2.setDescription(product.getDescription());
-						product2.setPrice(product.getPrice());
-						product2.setQuantity(product.getQuantity()+product2.getQuantity());
-						product2.setSize(product.getSize());
-						product2.setType(product.getType());
-						DataStock.update(product2);
-						isNew=false;
-					}
-					
-					
-				}if(isNew==true) {
-					DataStock.save(product);
-				}	
+		Product product = new Product(quantity, price, department, description, color, type, Size, category, Sku);
+		List<Object> Stock = DataStock.listItens();
+		boolean isNew = true;
+		for (int x = 0; x < Stock.size(); x++) {
+			Product product2 = (Product) Stock.get(x);
+			if (product.getSku() == product2.getSku()) {
+				product2.setCategory(product.getCategory());
+				product2.setColor(product.getColor());
+				product2.setDepartment(product.getDepartment());
+				product2.setDescription(product.getDescription());
+				product2.setPrice(product.getPrice());
+				product2.setQuantity(product.getQuantity() + product2.getQuantity());
+				product2.setSize(product.getSize());
+				product2.setType(product.getType());
+				DataStock.update(product2);
+				isNew = false;
+			}
+
+		}
+		if (isNew == true) {
+			DataStock.save(product);
+		}
 		return "Produto cadastrado com sucesso!";
 
 	}
+
 	public String listProduct() {
-		String exit= "Lista de produtos"+"\n";
-		List<Object> Stock= DataStock.listItens();
-		for(int x=0; x<Stock.size();x++) {
+		String exit = "Lista de produtos" + "\n";
+		List<Object> Stock = DataStock.listItens();
+		for (int x = 0; x < Stock.size(); x++) {
 			Product product = (Product) Stock.get(x);
-			exit+=product.toString();
-			
+			exit += product.toString();
+
 		}
-		
+
 		return exit;
 	}
-	
-	public String deleteProduct(int a) {
-	DataStock.delete(a);
+
+	public String deleteProduct(Sku a) {
+		List<Object> Stock = DataStock.listItens();
+
+		for (int x = 0; x < Stock.size(); x++) {
+			Product product2 = (Product) Stock.get(x);
+			if (a == product2.getSku()) {
+				DataStock.delete(x);
+
+			}
+
+		}
+
 		return "Objeto deletado";
 	}
 
